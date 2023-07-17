e94fe2d6da0a98d836be48819311a072734fc7ff
Dilshan-Dekumpitiya <dilshandekumpitiya@gmail.com>
2023-07-17 14:51:31 +0530

    change site_url to base_url

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -1,5 +1,5 @@
-<a href="<?= site_url('lang/en'); ?>">English</a>
-<a href="<?= site_url('lang/si'); ?>">Sinhala</a>
+<a href="<?= base_url('lang/en'); ?>">English</a>
+<a href="<?= base_url('lang/si'); ?>">Sinhala</a>
 
 <h1><?= lang("Text.page_title") ?></h1>
 <h2><?= lang("Text.welcome_message") ?></h2>
