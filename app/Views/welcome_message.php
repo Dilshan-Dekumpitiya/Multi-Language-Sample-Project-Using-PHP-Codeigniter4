6341912c669a4e4242d0551fbd7d923151f2aafc
Dilshan-Dekumpitiya <dilshandekumpitiya@gmail.com>
2023-07-17 13:48:23 +0530

    modify view

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -1,7 +1,8 @@
-<a class="dropdown-item" href="<?= site_url('lang/en'); ?>">English</a>
-<a class="dropdown-item" href="<?= site_url('lang/si'); ?>">Sinhala</a>
+<a href="<?= site_url('lang/en'); ?>">English</a>
+<a href="<?= site_url('lang/si'); ?>">Sinhala</a>
 
 <h1><?= lang("Text.page_title") ?></h1>
 <h2><?= lang("Text.welcome_message") ?></h2>
 <h3><?= lang("Text.author_information") ?></h3>
-  
+
+
