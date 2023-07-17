f7b49221e56bc2f8c9031abe9fc6aadf6efcad9a
Dilshan-Dekumpitiya <dilshandekumpitiya@gmail.com>
2023-07-17 08:53:20 +0530

    create language files to sinhala and english

---
+++ app/Language/en/Text.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'page_title' => 'Welcome Page',
+    'welcome_message' => 'Hi, Welcome to this page',
+    'author_information' => 'My name is Dilshan Dekumpitiya. This blog is mine and we created this post for to learn how to use multi language using codeiginter',
+];
